26ff5940fe60be4c55ffdc60b5caebf415869843
Vladimir <vovaykunin@gmail.com>
2023-05-23 16:56:31 +0300

    На проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+  public static void main(String[] args) {
+    task1();
+    task2();
+    task3();
+    task4();
+
+
+  }
+  public static void task1() {
+    List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    for (Integer num : nums) {
+      if (num % 2 == 1) {
+        System.out.println(num);
+      }
     }
+  }
+  public static void task2() {
+    List<Integer> nams = new ArrayList<>(List.of(10, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    Set<Integer> set = new TreeSet<>(nams);
+    for (Integer nam : set) {
+      if (nam % 2 == 0) {
+        System.out.println(nam);
+      }
+    }
+  }
+  public static void task3() {
+
+    List<String> stringList = new ArrayList<>(Arrays.asList("qwe","rty","asd","asd","wer"));
+    Set<String> set = new HashSet<>(stringList);
+    System.out.println(set);
+  }
+
+  public static void task4() {
+    List<String> words = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+    Map<String, Integer> map = new HashMap<>();
+    for (String word : words) {
+      if (map.containsKey(word)) {
+        Integer count = map.get(word)+1;
+        map.put(word, count);
+      } else {
+        map.put(word,1);
+      }
+    }
+    System.out.println(map.values());
+
+  }
 }
+
